59f3b7cbe8a64057d2bf2d9d01ded0f513aca8fd
Johnny Urosevic <TheMagicalCake@gmail.com>
2018-07-16 00:39:34 -0700

    Fixed post selecting query

--- snap.js
+++ snap.js
@@ -1,10 +1,10 @@
-let numPosts = $("div[class*='thing id-t3_8z']").length;
+let numPosts = $("div[class*='thing id-t3_8']").length;
 
 let half = numPosts / 2;
 
 for (let i = 0; i < half; i++) {
     let random = Math.floor(Math.random()*numPosts);
-    let element = $("div[class*='thing id-t3_8z']");
+    let element = $("div[class*='thing id-t3_8']");
     element.eq(random).fadeOut(5000, () => element.eq(random).css("display", "block").css("visibility", "hidden"));
     numPosts--;
 }
